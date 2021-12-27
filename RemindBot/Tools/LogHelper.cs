2971a31c7e39e81c7341a55af08f646cbea5a373
A2 <a2zjw@foxmail.com>
2021-12-27 11:27:53 +0800

    添加项目文件。

---
+++ RemindBot/Tools/LogHelper.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using log4net.Config;
+using log4net.Repository;
+using System.IO;
+
+namespace RemindBot
+{
+    public class LogHelper
+    {
+        private static ILoggerRepository repository { get; set; }
+        private static ILog _log;
+        private static ILog log
+        {
+            get
+            {
+                if (_log == null)
+                {
+                    Configure();
+                }
+                return _log;
+            }
+        }
+
+        public static void Configure(string repositoryName = "NETCoreRepository", string configFile = "log4net.config")
+        {
+            repository = LogManager.CreateRepository(repositoryName);
+            XmlConfigurator.Configure(repository, new FileInfo(configFile));
+            _log = LogManager.GetLogger(repositoryName, "");
+        }
+
+        public static void Info(string msg)
+        {
+            log.Info(msg);
+        }
+
+        public static void Warn(string msg)
+        {
+            log.Warn(msg);
+        }
+
+        public static void Error(string msg)
+        {
+            log.Error(msg);
+        }
+    }
+}
